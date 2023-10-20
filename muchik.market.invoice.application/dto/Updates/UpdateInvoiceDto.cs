069f6676a1042a50ff0d50dc05b21e0390b35086
Richard Augusto Bazan Ochoa <richardbazano@gmail.com>
2023-10-19 23:07:26 -0500

    Suscribe update state invoice and suscribe create transaction

---
+++ muchik.market.invoice.application/dto/Updates/UpdateInvoiceDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace muchik.market.invoice.application.dto.Updates
+{
+    public class UpdateInvoiceDto
+    {
+        public int InvoiceId { get; set; }
+        public int State { get; set; } 
+    }
+}
---
+++ muchik.market.transaction.application/events/CreateTransactionEvent.cs
@@ -0,0 +1,21 @@
+﻿using muchik.market.domain.events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace muchik.market.transaction.application.events
+{
+    public class CreateTransactionEvent : Event
+    {
+        public int InvoiceId { get; set; }
+        public Decimal Amount { get; set; }
+
+        public CreateTransactionEvent(int invoiceId, decimal amount)
+        {
+            InvoiceId = invoiceId;
+            Amount = amount;
+        }
+    }
+}
